0f9b2acb539155a03246497a0f8cdcb3f106a947
Zhang Huanjie <james@ustc.edu.cn>
2018-01-03 10:53:39 +0800

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
- mkdir -p /mnt/huge
- mount -t hugetlbfs nodev /mnt/huge
- echo 64 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
- 
- ip link set eth1 up
- ./simple-web -c1 -n1 --vdev=net_pcap0,iface=eth1 -- 222.195.81.233 80
+./simple-web -c1 -n1 -- 222.195.81.233 80
